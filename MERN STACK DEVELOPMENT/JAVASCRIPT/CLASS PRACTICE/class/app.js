a01430a458847dbe012276a7543b064866c4e66a
muhammadjunaidfarooq <junaidfarooq.pk@gmail.com>
2025-03-04 22:14:28 +0500

    JavaScript

--- MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
+++ MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
@@ -1,43 +1,8 @@
-// // Loops With array
-// let fruits = ["mango", "banana", "orange", "apple", "lechee", "grapes"];
-// let count = 1;
-// fruits.push("pineapple")
-// fruits.unshift("Gava")
-// for (let i=fruits.length-2; i>=0; i=i-2){
-//     console.log(i,fruits[i]);
-// }
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.SQRT1_2);
 
-// // loops with nested array
-// let heroes = [["ironman", "spiderman", "thor"], ["superman", "wonder woman", "flash"], ["ironman", "spiderman", "thor"]];
-// for (let i=0; i<heroes.length; i++) {
-//     console.log("List", heroes[i]);
-//     for (let j=0; j<heroes[i].length; j++) {
-//         console.log(heroes[i][j]);
-//     }
-// }
-
-// let students = [["Ali", 95], ["Ahmad", 100], ["Saif", 97]];
-// for (let i = 0; i < students.length; i++) {
-//     console.log(i, students[i]);
-//     for (let j = 0; j < students[i].length; j++) {
-//         console.log(students[i][j]);
-//     }
-// }
-
-// // for of loops
-// let fruits = ["mango", "banana", "orange", "apple", "lechee", "grapes"];
-// for (fruit of fruits) {
-//     console.log(fruit);
-// }
-
-// let str = "Muhammad Junaid Farooq";
-// for (char of str) {
-//     console.log(char);
-// }
-
-let heroes = [["ironman", "spiderman", "thor"], ["superman", "wonder woman", "flash"], ["ironman", "spiderman", "thor"]];
-for (list of heroes) {
-    for (hero of list) {
-        console.log(hero);
-    }
-}
+let x = 24;
+let y = 27;
+let z = 89;
+console.log(Math.pow(x, y, z));
---
+++ MERN STACK DEVELOPMENT/JAVASCRIPT/Creating a post/app.js
@@ -0,0 +1,14 @@
+let userName = "Muhammad Junaid Farooq";
+let contentOfPost = "“Are you ready to celebrate? Well, get ready: We have ICE!!!!! Yes, ICE, *WATER ICE* on Mars! woot!!! Best day every!!”"
+let likes = 22;
+let tag = ["NASA", "USA"];
+
+const post = {
+    username: userName,
+    content: contentOfPost, 
+    likes: likes,
+    repost: 150,
+    tags: tag
+}
+
+console.log(post);
