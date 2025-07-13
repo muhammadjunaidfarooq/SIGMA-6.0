570851d4a16d541bed3b6f6308ca87c458d02782
muhammadjunaidfarooq <junaidfarooq.pk@gmail.com>
2025-07-13 22:41:57 +0500

    JavaScript

--- MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
+++ MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
@@ -1,15 +1,7 @@
-let jsonRes = '{"fact":"The Maine Coon is 4 to 5 times larger than the Singapura, the smallest breed of cat.","length":84}';
-console.log(jsonRes);
-console.log(jsonRes.fact);
+let id = setInterval (() => {
+    console.log("Hi");
+},2000);
 
-let validRes = JSON.parse(jsonRes);
-console.log(validRes);
-console.log(validRes.fact);
-
-let student = {
-  name : "Junaid",
-  marks : "93"
-}
-
-// let objToJson = JSON.stringify(student);
-// console.log(objToJson);
+let id2 = setInterval(() => {
+    console.log("there!");
+}, 3000); 
