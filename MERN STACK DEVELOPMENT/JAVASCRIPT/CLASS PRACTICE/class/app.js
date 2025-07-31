e688787c7c37dbc38b14462d8ea5d65011297bdc
muhammadjunaidfarooq <junaidfarooq.pk@gmail.com>
2025-07-31 17:04:55 +0500

    Practic

--- MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
+++ MERN STACK DEVELOPMENT/JAVASCRIPT/CLASS PRACTICE/class/app.js
@@ -1,7 +1,21 @@
-let id = setInterval (() => {
-    console.log("Hi");
-},2000);
+let min = 5;
+let max = 12;
 
-let id2 = setInterval(() => {
-    console.log("there!");
-}, 3000); 
+let random = Math.random();
+console.log("Random: " + random);
+
+let diff = max - min;
+console.log("Difference of max - min : " + diff);
+
+let diffSum = diff;
+console.log("Adding 1 in difference: " + diffSum);
+
+let product = random * diffSum;
+console.log("Product of random and difference: " + product);
+
+
+
+
+let randomNumber = Math.floor(0.789727109 * (8-4 + 1)) + 4;
+                                                                                  
+console.log(randomNumber);                 
